13fbe0952597a12c804f206be9394011c82130ac
Cody Brunner <me@codybrunner.com>
2022-07-02 21:03:25 -0500

    feat(app): implements current weather

---
+++ src/components/Weather/Weather.tsx
@@ -0,0 +1,98 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { useCurrentWeather } from '@hooks/useCurrentWeather'
+import { State } from '@hooks/useUnits'
+import type { City } from '@interfaces/weather'
+import {
+	toCelsius,
+	toFahrenheit,
+	toKMH,
+	toMbars,
+	toMPH,
+	toPSI,
+} from '@utils/converters'
+import { toHumanReadableTime } from '@utils/datetime'
+import type { FC } from 'react'
+import { WiBarometer, WiHumidity, WiSunrise, WiSunset } from 'react-icons/wi'
+
+type Props = {
+	city: City
+	units: Pick<State, 'pressure' | 'speed' | 'temperature' | 'volume'>
+}
+
+export const Weather: FC<Props> = ({ city, units }) => {
+	const { data } = useCurrentWeather(city)
+	return (
+		<>
+			<h1 className='text-3xl'>
+				{data?.name}, {data?.country}
+			</h1>
+			<div className='indicator'>
+				<span className='p-2 text-lg shadow-lg indicator-item badge badge-error'>
+					{units.temperature === 'C' ? (
+						<span>{toCelsius(data!.tempMax)} C˚</span>
+					) : (
+						<span>{toFahrenheit(data!.tempMax)} F˚</span>
+					)}
+				</span>
+				<div className='grid place-items-center w-60 h-60 rounded-full shadow-lg bg-base-300'>
+					<span className='text-6xl'>
+						{units.temperature === 'C' ? (
+							<span>{toCelsius(data!.temp)} C˚</span>
+						) : (
+							<span>{toFahrenheit(data!.temp)} F˚</span>
+						)}
+					</span>
+				</div>
+				<span className='p-2 text-lg shadow-lg indicator-item indicator-start indicator-bottom badge badge-primary'>
+					{units.temperature === 'C' ? (
+						<span>{toCelsius(data!.tempMin)} C˚</span>
+					) : (
+						<span>{toFahrenheit(data!.tempMin)} F˚</span>
+					)}
+				</span>
+			</div>
+			<div className='flex space-x-4'>
+				<div className='flex items-center'>
+					<WiHumidity className='w-12 h-12' />
+					<span className='text-lg'>{data!.humidity} %</span>
+				</div>
+				<div className='flex space-x-4'>
+					{units.pressure === 'in' ? (
+						<div className='flex items-center'>
+							<WiBarometer className='w-12 h-12' />
+							<span>{toPSI(data!.pressure)} psi</span>
+						</div>
+					) : (
+						<div className='flex items-center'>
+							<WiBarometer className='w-12 h-12' />
+							<span>{toMbars(data!.pressure)} mbars</span>
+						</div>
+					)}
+				</div>
+				<div className='flex space-x-4'>
+					<div className='flex items-center'>
+						{units.speed === 'kmh' ? (
+							<span>{toKMH(data!.windSpeed)} km/h</span>
+						) : (
+							<span>{toMPH(data!.windSpeed)} mph</span>
+						)}
+					</div>
+				</div>
+			</div>
+			<div className='flex justify-center items-center space-x-8 w-full'>
+				<div className='flex items-center space-x-2'>
+					<WiSunrise className='w-12 h-12' />
+					<span className='text-lg'>
+						{toHumanReadableTime(data!.sunrise, city)}
+					</span>
+				</div>
+				<div className='flex items-center space-x-2'>
+					<WiSunset className='w-12 h-12' />
+					<span className='text-lg'>
+						{toHumanReadableTime(data!.sunset, city)}
+					</span>
+				</div>
+			</div>
+		</>
+	)
+}
